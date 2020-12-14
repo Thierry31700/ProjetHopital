031501f75f668e4eb5f040d1282492a455df8610
D <thierry.devillers@hotmail.fr>
2020-12-14 11:21:01 +0100

    maj  orig projet

--- src/main/java/config/Context.java
+++ src/main/java/config/Context.java
@@ -1,5 +1,43 @@
 package config;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+
 public class Context {
 
+	
+	
+	
+	private static Context _instance;
+	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("hopital");
+
+
+	//private IDAOAlbum daoAlbum= new DAOAlbumJPA();
+	//private IDAOBibliotheque daoBibliotheque = new DAOBibliothequeJPA();
+	//private IDAOFiche daoFiche = new DAOFicheJPA();
+	
+	
+	private Context() {}
+	
+	public static Context getInstance()
+	{
+		if(_instance==null) 
+		{
+			_instance=new Context();
+		}
+		return _instance;
+	}
+
+	public EntityManagerFactory getEmf() {
+		return emf;
+	}
+	public void closeEmf() 
+	{
+		emf.close();
+	}
+
+
+	
 }
