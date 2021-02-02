b8a718dd4a80bf525dd2d009f4225c8d67c52d65
D <thierry.devillers@hotmail.fr>
2021-02-02 12:04:32 +0100

    CongeRepo,ServiceRepo

---
+++ ProjetAJCSpringBoot/src/main/java/ProjetAJC/ProjetAJCSpringBoot/repository/CongeRepository.java
@@ -0,0 +1,9 @@
+package ProjetAJC.ProjetAJCSpringBoot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ProjetAJC.ProjetAJCSpringBoot.entity.Conge;
+
+public interface CongeRepository  extends JpaRepository<Conge, Integer> {
+
+}
