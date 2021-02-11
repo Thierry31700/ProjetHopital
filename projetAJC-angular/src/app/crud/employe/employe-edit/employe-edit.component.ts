9c4675b5c5f83f1f21ac3f0edd1e7457a823c4a6
D <thierry.devillers@hotmail.fr>
2021-02-11 15:25:05 +0100

    employeEdit

--- projetAJC-angular/src/app/crud/employe/employe-edit/employe-edit.component.ts
+++ projetAJC-angular/src/app/crud/employe/employe-edit/employe-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Employe } from 'src/app/model/employe';
+import { EmployeService } from 'src/app/service/employe.service';
 
 @Component({
   selector: 'app-employe-edit',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employe-edit.component.css']
 })
 export class EmployeEditComponent implements OnInit {
+employe:Employe = new Employe();
 
-  constructor() { }
+constructor(
+  private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private employeService: EmployeService,
+) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      if (params.id) {
+        this.employeService.findById(params.id).subscribe((data) => {
+          this.employe = data;
+        });
+      }
+    });
   }
+  public save() {
+    if (this.employe.id) {
+      this.employeService.update(this.employe).subscribe((result) => {
+        this.goList({ info: 'update' });
+      });
+    } else {
+      this.employeService.insert(this.employe).subscribe((result) => {
+        this.goList({ info: 'insert' });
+      });
+    }
+
+    private goList(info: Object) {
+      this.router.navigate(['/formation'], { queryParams: info });
+    }
+  }
+
+
+
 
 }
