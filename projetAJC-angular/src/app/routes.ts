376f3c27986d2ec49a20b7a28312d9191f99c5f6
D <thierry.devillers@hotmail.fr>
2021-02-11 14:52:21 +0100

    login loginService employeList

--- projetAJC-angular/src/app/routes.ts
+++ projetAJC-angular/src/app/routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from "@angular/router";
+import { EmployeEditComponent } from "./crud/employe/employe-edit/employe-edit.component";
+import { EmployeListComponent } from "./crud/employe/employe-list/employe-list.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'employe', component: EmployeListComponent },
+    { path: 'employe/edit', component: EmployeEditComponent },
+    { path: 'employe/edit/:id', component: EmployeEditComponent },
+    
     { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
--- projetAJC-angular/src/app/service/login.service.ts
+++ projetAJC-angular/src/app/service/login.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Login } from '../model/login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public auth(login: Login): Observable<void> {
+    const headers: HttpHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: 'Basic ' + btoa(`${login.username}:${login.password}`),
+    });
+    return this.http.get<void>('http://localhost:8080/monsite/api/login', {
+      headers: headers,
+    });
+  }
 }
+
